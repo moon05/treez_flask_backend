6fd6c20c561d367d9561485ff25fb81a3e4a39be
Abdullah Al Mamun <amamun@u.rochester.edu>
2020-11-16 23:32:25 -0500

    added sluglist, stuff to api and app

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import requests
 import pprint
+import pickle
+import random
 
 pp = pprint.PrettyPrinter()
 
@@ -37,25 +39,108 @@ def g_format(res_obj, keys):
 	res = {}
 	keys = keys
 	for i in keys:
-		res[i] = res_obj[i]
+		try:
+			res[i] = res_obj[i]
+		except:
+			print ("Error occured at getting key in g_format")
+			pass
+
 	return res
+
+def get_list_of_plants_from_main():
+	r = requests.get(base_url_plant, headers=HEADERS).json()
+	print(r.keys())
+	# pp.pprint(r["data"][8])
+	pp.pprint(r["data"])
+	print(r["links"])
+	print (len(r["data"]))
+
+	return
+
+
+
 
 def get_specifc_plant(plant_slug):
 	r = requests.get(base_url_plant+plant_slug, headers=HEADERS).json()
-	print(r["data"].keys())
-	g = g_format(r["data"])
+	print(r.keys())
+	pp.pprint(r["data"])
 
 	return
 
 def get_specific_species(species_slug):
-	r = requests.get(base_url_species+species_slug, headers=HEADERS).json()
+	print ("Slug delivered: " + species_slug)
+	r = requests.get(base_url_species+species_slug, headers=HEADERS)
+	print (r)
+	r= r.json()
 	
 	fields = ["common_name", "slug", "scientific_name", "year", "author", "family_common_name", "vegetable",
-				"image_url", "duration", "edible_part", "edible", "images", "common_names", "distributions", 
+				"image_url", "duration", "edible_part", "edible", "images", "common_names", 
 				"distribution", "flower", "fruit_or_seed", "specifications", "growth"]
 	
-	return g_format(r["data"], fields)
+	res = g_format(r["data"], fields)
+	
+	
 
+	if isinstance(res["common_names"], list):
+		print ("GOT a LIST in COMMON NAMES")
+		print (res["common_names"])
+	
+	if isinstance(res["common_names"], dict):
+		
+		t = res["common_names"].keys()
+		if ("eng" in t):
+			res["common_names"] = res["common_names"]["eng"]
+
+		elif ("en" in t):
+			res["common_names"] = res["common_names"]["en"]
+
+	# if common_name field is empty get the first one from common_names and remove it
+	# or if common_name is there and the first of common_names is the same as common_name
+	# remove it
+	if res["common_name"] is None:
+		print ("got in none")
+		res["common_name"] = res["common_names"].pop(0)
+	elif res["common_name"].lower() == res["common_names"][0].lower():
+		print ("got in both first equal")
+		res["common_names"].pop(0)
+	
+	if isinstance(res["distribution"], list):
+		print (res["distribution"])
+		res["distribution"].sort()
+		print ("GOTCHA in DIST")
+
+	if isinstance(res["distribution"], dict):
+		if "native" in res["distribution"].keys():
+			res["distribution"] = res["distribution"]["native"]
+			res["distribution"].sort()
+
+	if "cm" in res["specifications"]["maximum_height"].keys():
+		res["max_height"] = res["specifications"]["maximum_height"]["cm"]
+
+
+	pp.pprint(res)
+
+
+def get_tree_slug_list(count):
+	
+	fields = ["common_name", "slug", "scientific_name", "year", "author", "family_common_name",
+				"image_url", "duration", "edible_part", "edible", "images", "common_names", 
+				"distribution", "flower", "specifications", "growth"]
+	final_list = []
+	if (count is 1):
+		
+		INITIAL_PARAMS = {"filter_not[images]": "null"}
+		r = requests.get(base_url_species, params=INITIAL_PARAMS,headers=HEADERS).json()
+
+	else:
+		INITIAL_PARAMS = {"filter_not[images]": "null", "page": 2 }
+		r = requests.get(base_url_species, params=INITIAL_PARAMS,headers=HEADERS).json()
+
+	for i in r["data"]:
+		final_list.append(i["slug"])
+
+
+	return {"random20plants": final_list}
 
 def top_tallest_trees():
 	r = requests.get("https://trefle.io/api/v1/plants?filter_not%5Bmaximum_height_cm%5D=null&filter%5Bligneous_type%5D=tree&order%5Bmaximum_height_cm%5D=desc", headers=headers).json()
@@ -84,8 +169,46 @@ def get_plants_from_specifc_country(country_code):
 	pp.pprint(r["data"][2].keys())
 
 
+def get_slugs_in_list():
+	final_list = []
+
+	INITIAL_PARAMS = {"filter_not[images]": "null"}
+	r = requests.get(base_url_species, params=INITIAL_PARAMS,headers=HEADERS).json()
+
+	for i in r["data"]:
+		final_list.append(i["slug"])
+
+	for j in range(2, 50, 1):
+		INITIAL_PARAMS = {"filter_not[images]": "null", "page": j }
+		r = requests.get(base_url_species, params=INITIAL_PARAMS,headers=HEADERS).json()
+		#adding all the slugs to final list from each page request
+		for k in r["data"]:
+			final_list.append(k["slug"])
+
+	with open("./slugList.txt", "wb") as fp:
+		pickle.dump(final_list, fp)
+
+	return final_list
 # get_specifc_plant("pinus-monticola")
-# get_specific_species("pinus-monticola")
+
+# get_specific_species("holcus-lanatus")
+# # get_list_of_plants_from_main()
+# x = get_tree_slug_list(1)
+# print (x)
+
+def load_slugs():
+	with open("./slugList.txt", "rb") as fp:
+		k = pickle.load(fp)
+		print (type(k))
+	print (random.sample(k, 1))
 
 
+# load_slugs()
 # height()
+
+# get_specific_species("juniperus-communis-var-communis")
+# get_specific_species("papaver-rhoeas")
+# get_specific_species("crepis-paludosa")
+# get_specific_species("hylotelephium-telephium")
+
+# pp.pprint (get_specific_species("juncus-acutiflorus"))
--- app.py
+++ app.py
@@ -1,25 +1,53 @@
 import os
 from flask import Flask
+from flask_cors import CORS, cross_origin
+from flask import request
 import api
 import pprint
+import pickle
+import random
 
 pp = pprint.PrettyPrinter()
 
 
-
 app= Flask(__name__)
-app.config.from_object('config')
+CORS(app)
+app.config.from_object('config', )
+app.config["CORS_HEADERS"] = "Content-Type"
+
+def load_slugs():
+	with open("./slugList.txt", "rb") as fp:
+		k = pickle.load(fp)
+		return k
+
+SLUG_LIST = load_slugs()
 
 @app.route('/')
 def index():
 	return ("<h1> You have reached the Treez API Page </h1>")
 
 
-@app.route('/getCountrySpecificPlant')
+@app.route('/getTreeSlug')
+@cross_origin()
+def getTreeSlug():
+	return {"slug": random.sample(SLUG_LIST, 1)[0]}
+
+@app.route('/getSpecificSpecies')
+@cross_origin()
 def getCountrySpecificPlant():
-	return api.get_specific_species("pinus-monticola")
-	# return get_specific_species("pinus-monticola")
-	# return {"Country_Specific_Plant_1": 1, "Country_Specific_Plant_2": 2}
+	try:
+		recSlug = str(request.args.get("slug"))
+		print (recSlug)
+	except:
+		#change with a better looking species
+		return api.get_specific_species("juniperus-communis-var-communis")
+
+	return api.get_specific_species(recSlug)
+
+
+@app.route('/testAnotherButton')
+def testAnotherButton():
+	return api.get_specific_species("holcus-lanatus")
 
 @app.route('/getRandomPlantFromAnywhere')
 def getRandomPlantFromAnywhere():
@@ -35,5 +63,5 @@ def getRandomPlantFromAnywhere():
 # Or specify port manually:
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 3035))
     app.run(host='0.0.0.0', port=port)
